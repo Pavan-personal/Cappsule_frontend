d00703d42cbb3dc7d3afec986118a7f7dc17f8e5
Pavan Kumar Anupoju <145895808+Pavan-personal@users.noreply.github.com>
2024-05-16 15:18:46 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
         }}
       >
         <div className="relative bg-red-400 w-fit m-4 my-8">
-          <div className="absolute inset-y-0 start-[6.m] flex items-center ps-3 pointer-events-">
+          <div className="absolute inset-y-0 start-[1.4rem] flex items-center ps-3 pointer-events-">
             {globalSearch === "" ? (
               <svg
                 className="w-5 h-5 text-gray-600 font-bold"
